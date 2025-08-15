2e83e3ac7dbbae4b87c9158d8b12f17923acfe5c
Yigitbdk <yigitbadik@gmail.com>
2025-08-15 15:45:17 +0300

    //

--- BlogApp/Validators/ForgotPasswordDtoValidator.cs
+++ BlogApp/Validators/ForgotPasswordDtoValidator.cs
@@ -16,6 +16,6 @@ namespace BlogApp.Validators
                 .NotEmpty().WithMessage("Email is required")
                 .EmailAddress().WithMessage("Invalid email format")
                 .MaximumLength(100).WithMessage("Email cannot exceed 100 characters");
-        }
+        }//
     }
 }
